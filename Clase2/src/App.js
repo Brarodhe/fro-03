010846e832999e70dbfec10e2af3f0939b2dcc4e
Brayan <Brarodhe@gmail.com>
2023-03-11 11:56:33 -0600

    Segunda clase
    React. Clases.

---
+++ Clase2/src/App.js
@@ -0,0 +1,16 @@
+import Clases from "./Clases";
+import Funciones from "./Funciones";
+
+function App() {
+    return ( <
+        div >
+        <
+        Clases / >
+        <
+        Funciones / >
+        <
+        /div>
+    );
+}
+
+export default App;
